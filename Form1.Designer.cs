dc8317ea481592930c15df42324b7aa5fa199c5a
harpytale <strayker_1747@mail.ru>
2016-02-29 16:58:59 +0300

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox10 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox11 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox12 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox13 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(77, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(142, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(207, 12);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(12, 76);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox5.TabIndex = 4;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(77, 76);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox6.TabIndex = 5;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(142, 76);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox7.TabIndex = 6;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(207, 76);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox8.TabIndex = 7;
+            this.richTextBox8.Text = "";
+            // 
+            // richTextBox9
+            // 
+            this.richTextBox9.Location = new System.Drawing.Point(12, 140);
+            this.richTextBox9.Name = "richTextBox9";
+            this.richTextBox9.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox9.TabIndex = 8;
+            this.richTextBox9.Text = "";
+            // 
+            // richTextBox10
+            // 
+            this.richTextBox10.Location = new System.Drawing.Point(77, 140);
+            this.richTextBox10.Name = "richTextBox10";
+            this.richTextBox10.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox10.TabIndex = 9;
+            this.richTextBox10.Text = "";
+            // 
+            // richTextBox11
+            // 
+            this.richTextBox11.Location = new System.Drawing.Point(142, 140);
+            this.richTextBox11.Name = "richTextBox11";
+            this.richTextBox11.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox11.TabIndex = 10;
+            this.richTextBox11.Text = "";
+            // 
+            // richTextBox12
+            // 
+            this.richTextBox12.Location = new System.Drawing.Point(207, 140);
+            this.richTextBox12.Name = "richTextBox12";
+            this.richTextBox12.Size = new System.Drawing.Size(59, 58);
+            this.richTextBox12.TabIndex = 11;
+            this.richTextBox12.Text = "";
+            // 
+            // richTextBox13
+            // 
+            this.richTextBox13.Location = new System.Drawing.Point(12, 225);
+            this.richTextBox13.Name = "richTextBox13";
+            this.richTextBox13.Size = new System.Drawing.Size(346, 84);
+            this.richTextBox13.TabIndex = 12;
+            this.richTextBox13.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 321);
+            this.Controls.Add(this.richTextBox13);
+            this.Controls.Add(this.richTextBox12);
+            this.Controls.Add(this.richTextBox11);
+            this.Controls.Add(this.richTextBox10);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Крестики-Нолики";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.RichTextBox richTextBox10;
+        private System.Windows.Forms.RichTextBox richTextBox11;
+        private System.Windows.Forms.RichTextBox richTextBox12;
+        private System.Windows.Forms.RichTextBox richTextBox13;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        private NetworkStream stream;
+        private TcpClient connection;
+        private Thread outputThread;
+        private BinaryWriter writer;
+        private BinaryReader reader;
+        private Square[] board;
+        private Square currentSquare;
+        private char point;
+        private bool turn;
+        private SolidBrush brush;
+        private bool done = false;
+        private void Client_Load(object sender, EventArgs e)
+        {
+            board = new Square[4, 4];
+            board[0, 0] = new Square(board0Panel, ' ', 0);
+            board[0, 1] = new Square(board1Panel, ' ', 1);
+            board[0, 2] = new Square(board2Panel, ' ', 2);
+            board[0, 3] = new Square(board3Panel, ' ', 3);
+            board[1, 0] = new Square(board4Panel, ' ', 4);
+            board[1, 1] = new Square(board5Panel, ' ', 5);
+            board[1, 2] = new Square(board6Panel, ' ', 6);
+            board[1, 3] = new Square(board7Panel, ' ', 7);
+            board[2, 0] = new Square(board8Panel, ' ', 8);
+            board[2, 1] = new Square(board9Panel, ' ', 9);
+            board[2, 2] = new Square(board10Panel, ' ', 10);
+            board[2, 3] = new Square(board11Panel, ' ', 11);
+            board[3, 0] = new Square(board12Panel, ' ', 12);
+            board[3, 1] = new Square(board13Panel, ' ', 13);
+            board[3, 2] = new Square(board14Panel, ' ', 14);
+            board[3, 3] = new Square(board15Panel, ' ', 15);
+
+            brush = new SolidBrush(Color.Black);
+            connection = new TcpClient("127.0.0.1", 50000);
+            stream = connection.GetStream();
+            writer = new BinaryWriter(stream);
+            reader = new BinaryReader(stream);
+            outputThread = new Thread(new ThreadStart(Run));
+            outputThread.Start();
+        }
+        private void Client_paint(object sender,
+           PaintEventArgs e)
+        {
+            PaintSquares();
+        }
+        private void Client_close(object sender,
+           FormClosingEventArgs e)
+        {
+            done = true;
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+        private delegate void DisplayDelegate(string message);
+        private void DisplayMessage(string message)
+        {
+            if (displayTextBox.InvokeRequired)
+            {
+                Invoke(new DisplayDelegate(DisplayMessage),
+                   new object[] { message });
+            }
+            else
+                displayTextBox.Text += message;
+        }
+        private delegate void ChangeIdLabelDelegate(string message);
+        private void ChangeIdLabel(string label)
+        {
+            if (idLabel.InvokeRequired)
+            {
+                Invoke(new ChangeIdLabelDelegate(ChangeIdLabel),
+                   new object[] { label });
+            }
+            else
+                idLabel.Text = label;
+        }
+        public void PaintSquares()
+        {
+            Graphics g;
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; column++)
+                {
+                    g = board[row, column].SquarePanel.CreateGraphics();
+                    g.DrawString(board[row, column].Mark.ToString(),
+                       board0Panel.Font, brush, 10, 8);
+                }
+            }
+        }
+        private void square_MouseUp(object sender,
+           System.Windows.Forms.MouseEventArgs e)
+        {
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; column++)
+                {
+                    if (board[row, column].SquarePanel == sender)
+                    {
+                        CurrentSquare = board[row, column];
+                        SendClickedSquare(board[row, column].Location);
+                    }
+                }
+            }
+        }
+        public void Run()
+        {
+
+            point = reader.ReadChar();
+            ChangeIdLabel("Игрок\"" + point + "\"");
+            turn = (point == 'X' ? true : false);
+            try
+            {
+                while (!done)
+                    ProcessMessage(reader.ReadString());
+            }
+
+
+    }
+        public void ProcessMessage(string message)
+        {
+            if (message == "...")
+            {
+                currentSquare.Mark = point;
+                PaintSquares();
+            }
+            else if (message == "Неверный ход. Попрбуйте снова")
+            {
+                DisplayMessage(message + "\r\n");
+                turn = true;
+            }
+            else if (message == "Ход противника.")
+            {
+
+                int location = reader.ReadInt32();
+                board[location / 3, location % 3].Mark =
+                   (point == 'X' ? 'O' : 'X');
+                PaintSquares();
+
+                DisplayMessage("Ваш ход.\r\n");
+                turn = true;
+            }
+            else
+                DisplayMessage(message + "\r\n");
+        }
+
+
+        public void SendClickedSquare(int location)
+        {
+            if (turn)
+            {
+                writer.Write(location);
+                turn = false;
+            }
+        }
+        public Square CurrentSquare
+        {
+            set
+            {
+                currentSquare = value;
+            }
+        }
+    }
+
+
+
